bd7a6a5a017ed71da9001d1b6d70907bfd3d4490
David Ostrovsky <david@ostrovsky.org>
2018-08-05 08:55:06 +0200

    Use RestApiServlet and SSH commands for plugin actions
    This change is adding one endpoint with three methods:
    
    * GET /config/server/readonly~readonly
    * PUT /config/server/readonly~readonly
    * DELETE /config/server/readonly~readonly
    
    and based on this endpoint, three SSH commands:
    
    * ssh host readonly get
    * ssh host readonly put
    * ssh host readonly delete
    
    Change-Id: I1797906aebb30674c7c37ecfe03a8b944b631299

---
+++ src/main/java/com/googlesource/gerrit/plugins/readonly/ReadOnlyEndpoint.java
@@ -0,0 +1,81 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.readonly;
+
+import static com.google.gerrit.common.data.GlobalCapability.ADMINISTRATE_SERVER;
+import static com.google.gerrit.common.data.GlobalCapability.MAINTAIN_SERVER;
+
+import com.google.gerrit.extensions.annotations.RequiresAnyCapability;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.server.config.ConfigResource;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.io.IOException;
+
+public class ReadOnlyEndpoint {
+  static class Input {}
+
+  @RequiresAnyCapability({ADMINISTRATE_SERVER, MAINTAIN_SERVER})
+  @Singleton
+  public static class Get implements RestReadView<ConfigResource> {
+    private final ReadOnlyState state;
+
+    @Inject
+    Get(ReadOnlyState state) {
+      this.state = state;
+    }
+
+    @Override
+    public String apply(ConfigResource resource) {
+      return state.isReadOnly() ? "on" : "off";
+    }
+  }
+
+  @RequiresAnyCapability({ADMINISTRATE_SERVER, MAINTAIN_SERVER})
+  @Singleton
+  public static class Put implements RestModifyView<ConfigResource, Input> {
+    private final ReadOnlyState state;
+
+    @Inject
+    Put(ReadOnlyState state) {
+      this.state = state;
+    }
+
+    @Override
+    public Response<String> apply(ConfigResource resource, Input input) throws IOException {
+      state.setReadOnly(true);
+      return Response.ok("");
+    }
+  }
+
+  @RequiresAnyCapability({ADMINISTRATE_SERVER, MAINTAIN_SERVER})
+  @Singleton
+  public static class Delete implements RestModifyView<ConfigResource, Input> {
+    private final ReadOnlyState state;
+
+    @Inject
+    Delete(ReadOnlyState state) {
+      this.state = state;
+    }
+
+    @Override
+    public Response<String> apply(ConfigResource resource, Input input) throws IOException {
+      state.setReadOnly(false);
+      return Response.ok("");
+    }
+  }
+}
